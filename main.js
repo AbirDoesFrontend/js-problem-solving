6a362401fb879dd66c2fda63b6dc61105d36d34c
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-07-25 12:31:04 +0600

    Task - 01 - Completed

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Js Problem Solving')
+// Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+const stringReverser = str => {
+    let reversedStr = "";
+
+    for(let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str.charAt(i);
+    }
+    return reversedStr;
+}
+
+const inputValue = 'hello world';
+const outputValue = stringReverser(inputValue);
+
